c2b0bcc08de2c5e0ea3aafbbc636b1cc46505b95
Fanny Vasquez <fannyvr18@gmail.com>
2016-06-23 17:23:12 -0700

    Add: CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var HTMLWebpackPlugin = new HtmlWebpackPlugin({
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
@@ -10,17 +10,21 @@ module.exports = {
     './app/index.js'
   ],
   module: {
-    loarders: [
+    loaders: [
       {
         test: /\.js$/, 
         exclude: /node_modules/, 
-        loarder: 'babel-loader'
-      }
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+      },
     ]
   },
   output: {
     filename: 'index_bundle.js',
     path: __dirname + '/dist'
   },
-  plugins: [HTMLWebpackPlugin]
+  plugins: [HtmlWebpackPluginConfig]
 };
