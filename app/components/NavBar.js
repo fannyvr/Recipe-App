e93709b9d90e35c573678b8413c5081e6e992ed4
Fanny <fannyvr18@gmail.com>
2016-09-02 19:01:57 -0700

    syntax(navbar): change component to stateless function

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,19 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-DOM';
 import {hashHistory, IndexRoute} from 'react-router';
 import '../styles/navBar.css';
 
-class NavBar extends Component{
-  render (){
-    return(
-      <div>
-        <ul>
-          <li><a onClick= {()=> hashHistory.push({IndexRoute})}> My Recipes </a> </li>
-          <li><a onClick= {()=> hashHistory.push('/searchrecipes')}> Search Recipes </a></li>
-        </ul>
-      </div>
-    )
-  }
-}
+const NavBar = () => (
+  <div>
+	  <ul>
+		  <li><a onClick= {()=> hashHistory.push({IndexRoute})}> My Recipes </a> </li>
+		  <li><a onClick= {()=> hashHistory.push('/searchrecipes')}> Search Recipes </a></li>
+	  </ul>
+  </div>
+ )
 
 export default NavBar; 
