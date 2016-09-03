f9c44421d3959dbe038ddc8d702ebb8cf103cf44
Fanny <fannyvr18@gmail.com>
2016-09-02 19:29:15 -0700

    feat(signout): add signout route to react router

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,14 +3,15 @@ import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import AppContainer from '../containers/AppContainer';
 import SearchRecipes from '../components/SearchRecipes';
 import MyRecipes from '../components/MyRecipes';
+import Signedout from '../components/Signedout';
 
 const routes = (
   <Router history={hashHistory}>
     <Route path="/" component={AppContainer}>
       <IndexRoute component={MyRecipes}/>
       <Route path="/searchrecipes" component={SearchRecipes}/>
+      <Route path="/signedout" component={Signedout}/>
     </Route>
   </Router>
 )
-
 export default routes;
