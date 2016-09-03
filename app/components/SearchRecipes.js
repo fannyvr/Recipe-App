ab4b1e37dc1b95bc24c83a7778b72e7d227f11b7
Fanny <fannyvr18@gmail.com>
2016-09-02 18:53:22 -0700

    syntax(recipe search): change component to stateless function

--- app/components/SearchRecipes.js
+++ app/components/SearchRecipes.js
@@ -1,14 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class SearchRecipes extends Component{
-  render (){
-    return(
-      <div>
-        Recipe Search Content
-      </div>
-    )
-  }
-}
+const SearchRecipes = () => (
+  <div>
+    Recipe Search Content
+  </div>
+)
 
 export default SearchRecipes;
