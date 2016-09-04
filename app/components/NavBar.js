5ad7467dad7c9f44a47ee085d34a89c1da0db8af
Fanny <fannyvr18@gmail.com>
2016-09-03 17:28:33 -0700

    syntax(navBar): fix indentation to two spaces

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -5,11 +5,11 @@ import '../styles/navBar.css';
 
 const NavBar = () => (
   <div>
-	  <ul className="nav">
-		  <li className="navButton"><a onClick= {()=> hashHistory.push({IndexRoute})}> My Recipes </a> </li>
-		  <li className="navButton"><a onClick= {()=> hashHistory.push('/searchrecipes')}> Search Recipes </a></li>
-		  <li className="navButton"><a onClick= {()=> hashHistory.push('/signedout')}> Sign Out </a></li>
-	  </ul>
+    <ul className="nav">
+      <li className="navButton"><a onClick= {()=> hashHistory.push({IndexRoute})}> My Recipes </a> </li>
+      <li className="navButton"><a onClick= {()=> hashHistory.push('/searchrecipes')}> Search Recipes </a></li>
+      <li className="navButton"><a onClick= {()=> hashHistory.push('/signedout')}> Sign Out </a></li>
+    </ul>
   </div>
  )
 
