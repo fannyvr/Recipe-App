1306a815d1432868a6ec709d50d491eff65f2181
Fanny <fannyvr18@gmail.com>
2016-09-06 21:54:24 -0700

    refactor(components): Create a folder for each component and move css files to corresponding component folder

---
+++ app/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-DOM';
+import {hashHistory, IndexRoute} from 'react-router';
+import './navBar.css';
+
+const NavBar = () => (
+  <div>
+    <ul className="nav">
+      <li className="navButton"><a onClick= {()=> hashHistory.push({IndexRoute})}> My Recipes </a> </li>
+      <li className="navButton"><a onClick= {()=> hashHistory.push('/searchrecipes')}> Search Recipes </a></li>
+      <li className="navButton"><a onClick= {()=> hashHistory.push('/signedout')}> Sign Out </a></li>
+    </ul>
+  </div>
+ )
+
+export default NavBar; 
---
+++ app/components/Search/SearchRecipes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SearchRecipes = () => (
+  <div>
+    Recipe Search Content
+  </div>
+)
+
+export default SearchRecipes;
