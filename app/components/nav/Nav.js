2f45da67ef561858c489e567c88b6618898b9400
Fanny <fannyvr18@gmail.com>
2016-09-11 16:35:04 -0700

    chore(nav): changed file names from NavBar/NavBar.js & /NavBar/navBar.css to app/components/nav/Nav.js & app/components/nav/nav.css

---
+++ app/components/nav/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-DOM';
+import {hashHistory, IndexRoute} from 'react-router';
+import './nav.css';
+
+const Nav = () => (
+  <div>
+    <ul className="nav">
+      <li className="navButton"><a onClick= {()=> hashHistory.push({IndexRoute})}> My Recipes </a> </li>
+      <li className="navButton"><a onClick= {()=> hashHistory.push('/searchrecipes')}> Search Recipes </a></li>
+      <li className="navButton"><a onClick= {()=> hashHistory.push('/signedout')}> Sign Out </a></li>
+    </ul>
+  </div>
+ )
+
+export default Nav; 
+
