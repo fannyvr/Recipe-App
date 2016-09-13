c386e87a59d386907c6d2d31c280c2d1cfb0dfb0
Fanny <fannyvr18@gmail.com>
2016-09-12 19:43:31 -0700

    feat(Search): added from with fields: Main Ingredient, Number of Recipes and Type of Meal

--- app/components/search/Search.js
+++ app/components/search/Search.js
@@ -3,7 +3,32 @@ import ReactDOM from 'react-dom';
 
 const Search = () => (
   <div>
-    Recipe Search Content
+    <form>
+      <br />
+      Main Ingredient <br />
+      <input type="text" placeholder="Beef" />
+      <br /> <br />
+      Type of Meal <br/>
+      <select>
+        <option value="breakfast"> Breakfast </option>
+        <option value="lunch"> Lunch </option>
+        <option value="dinner"> Dinner </option>
+      </select>
+      <br /> <br />
+      Number of Recipes <br/>
+      <select>
+        <option value="1"> 1 </option>
+        <option value="2"> 2 </option>
+        <option value="3"> 3 </option>
+        <option value="4"> 4 </option>
+        <option value="5"> 5 </option>
+        <option value="6"> 6 </option>
+        <option value="7"> 7 </option>
+        <option value="8"> 8 </option>
+        <option value="9"> 9 </option>
+        <option value="10"> 10 </option>
+      </select>
+    </form>
   </div>
 )
 
