acc9a5361c7253c8b1b603487af16c653c00b31e
Fanny <fannyvr18@gmail.com>
2016-09-14 20:54:49 -0700

    feat(search): created component(container) to hold the logic for search

---
+++ app/containers/SearchContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Search from '../components/search/Search';
+
+class SearchContainer extends Component {
+  render(){
+    return(
+      <div>
+        <Search />
+      </div>
+    )
+  }
+}
+
+export default SearchContainer;
