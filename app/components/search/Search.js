776725cef8cfe7c418c398410736eb91c420224d
Fanny <fannyvr18@gmail.com>
2016-09-17 18:53:01 -0700

    syntax(): added spaces between params and curly braces for readability

--- app/components/search/Search.js
+++ app/components/search/Search.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const Search = (props) => (
+const Search = ( props ) => (
   <div>
     <form>
       <br />
       Main Ingredient <br />
-      <input type="text" onChange={props.onChange('ingredient')} value={props.enterIngredient} />
+      <input type="text" onChange={ props.onChange('ingredient') } value={ props.enterIngredient } />
       <br /> <br />
       Type of Meal <br/>
-      <select onChange={props.onChange('meal')} value={props.mealChoice}>
+      <select onChange={ props.onChange('meal') } value={ props.mealChoice }>
         <option value="breakfast"> Breakfast </option>
         <option value="lunch"> Lunch </option>
         <option value="dinner"> Dinner </option>
       </select>
       <br /> <br />
       Number of Recipes <br/>
-      <select onChange={props.onChange('number')} value={props.numberOfRecipes}>
+      <select onChange={ props.onChange('number') } value={ props.numberOfRecipes }>
         <option value="1"> 1 </option>
         <option value="2"> 2 </option>
         <option value="3"> 3 </option>
--- app/containers/SearchContainer.js
+++ app/containers/SearchContainer.js
@@ -1,29 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Search from '../components/search/Search';
 
 const SearchContainer = React.createClass({
   
   getInitialState(){
-    return {ingredient: ' ', meal: 'Breakfast', number: 1}
+    return { ingredient: ' ', meal: 'Breakfast', number: 1 }
   },
   
-  handleChange(keyword){
-    return (event) => {
+  handleChange( keyword ){
+    return ( event ) => {
       const state = {};
       state[keyword] = event.target.value;
-      this.setState(state);
+      this.setState( state );
     }
   },
 
   render(){
     return(
       <div>
-        <Search onChange={this.handleChange} 
-                enterIngredient={this.state.ingredient} 
-                onChange={this.handleChange}
-                mealChoice={this.state.meal}
-                onChange={this.handleChange}
-                numberOfRecipes={this.state.number} />
+        <Search onChange={ this.handleChange } 
+                enterIngredient={ this.state.ingredient } 
+                onChange={ this.handleChange }
+                mealChoice={ this.state.meal }
+                onChange={ this.handleChange }
+                numberOfRecipes={ this.state.number } />
       </div>
     )
   }
